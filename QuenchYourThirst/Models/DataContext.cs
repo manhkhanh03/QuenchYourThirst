13dd83280d698812e7d2fe72a081ca809b7c46be
phamthuylinhh <115873781+phamthuylinhh@users.noreply.github.com>
2023-10-29 06:11:44 +0700

    add

--- QuenchYourThirst/Models/DataContext.cs
+++ QuenchYourThirst/Models/DataContext.cs
@@ -1,12 +1,8 @@
-<<<<<<< Updated upstream
-﻿using Microsoft.EntityFrameworkCore;
-=======
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using Newtonsoft.Json.Linq;
 using QuenchYourThirst.Models;
 using System;
->>>>>>> Stashed changes
 
 namespace QuenchYourThirst.Models
 {
@@ -24,41 +20,10 @@ namespace QuenchYourThirst.Models
 
         public DbSet<ProductCategory> ProductCategorys { get; set; }
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            modelBuilder.Entity<ProductSizeFlavor>()
-                .HasOne(psf => psf.Product)
-                .WithMany(p => p.ProductSizeFlavor)
-                .HasForeignKey(psf => psf.product_id);
-
-            modelBuilder.Entity<ProductSizeFlavor>()
-                .HasOne(psf => psf.Size)
-                .WithMany()
-                .HasForeignKey(psf => psf.size_id);
-
-            modelBuilder.Entity<ProductSizeFlavor>()
-                .HasOne(psf => psf.Flavor)
-                .WithMany()
-                .HasForeignKey(psf => psf.flavor_id);
-
-            modelBuilder.Entity<Product>()
-                .HasMany(p => p.ProductImage)
-                .WithOne(pi => pi.Product)
-                .HasForeignKey(pi => pi.product_id);
-
-            modelBuilder.Entity<Product>()
-                .HasOne(sp => sp.StatusProduct)
-                .WithMany(p => p.Product)
-                .HasForeignKey(p => p.status_product_id);
-            modelBuilder.Entity<Product>()
-                .HasOne(pc => pc.ProductCategory)
-                .WithMany(p => p.Product)
-                .HasForeignKey(pci => pci.product_category_id);
-        }
         public DbSet<Bangtenweb> Bangtenwebs { get; set; }
+        //Đổi lại ten ni cho t cấy này, không có tiếng việt trong code bangtenweb ????
         public DbSet<Menu> Menus { get; set; }
         public DbSet<Postss> Postss { get; set; }
       
->>>>>>> Stashed changes
     }
 }
